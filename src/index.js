55555f26c67a49ca443aad35553e76a6c4978542
Kegan <44252449+Kronholt@users.noreply.github.com>
2022-01-26 06:01:34 -0600

    Added JSX for the list items

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const firstHottestPepper = "Carolina Reaper (2.2m SHU)";
+const secondHottestPepper = "Trinidad Moruga Scorpion (2m SHU)";
+const thirdHottestPepper = "7 Pot Douglah (1.8m SHU)";
+
 ReactDOM.render(
   <div>
     <h1>World's Hottest Peppers</h1>
     <ul>
-      <li>Carolina Reaper (2.2m SHU)</li>
-      <li>Trinidad Moruga Scorpion (2m SHU)</li>
-      <li>7 Pot Douglah (1.8m SHU)</li>
+      <li>{firstHottestPepper}</li>
+      <li>{secondHottestPepper}</li>
+      <li>{thirdHottestPepper}</li>
     </ul>
   </div>,
   document.getElementById("root")
