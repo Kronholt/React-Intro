dc3785c2c85b80e1bf5b832f7aeababb6c8932cb
Kegan <44252449+Kronholt@users.noreply.github.com>
2022-01-27 05:51:38 -0600

    Added shu rating to peppers

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -3,10 +3,11 @@ import { peppers } from "../data";
 
 const ListItem = (props) => {
   const pepper = props.pepper;
-  console.log(pepper);
   return (
     <li key={pepper.id}>
-      <h2>{pepper.name}</h2>
+      <h2>
+        {pepper.name} - {pepper.shu}
+      </h2>
       <img className="pepper-img" src={pepper.src} alt={pepper.name} />
     </li>
   );
--- src/data.js
+++ src/data.js
@@ -3,16 +3,19 @@ export const peppers = [
     id: 1,
     name: "Carolina Reaper",
     src:
-      "https://cdn11.bigcommerce.com/s-f6cx4kq/images/stencil/1280x1280/products/129/2297/red_carolina_reaper_pepper_tyler_farms_seeds_and_plants_3__78942.1578188399.JPG?c=2"
+      "https://cdn11.bigcommerce.com/s-f6cx4kq/images/stencil/1280x1280/products/129/2297/red_carolina_reaper_pepper_tyler_farms_seeds_and_plants_3__78942.1578188399.JPG?c=2",
+    shu: "2.2m SHU"
   },
   {
     id: 2,
     name: "Trinidad Moruga Scorpion",
-    src: "https://i.ytimg.com/vi/By-Q5TOude8/maxresdefault.jpg"
+    src: "https://i.ytimg.com/vi/By-Q5TOude8/maxresdefault.jpg",
+    shu: "2m SHU"
   },
   {
     id: 3,
     name: "7 Pot Douglah",
-    src: "https://m.media-amazon.com/images/I/41vKhXlhfGL._AC_.jpg"
+    src: "https://m.media-amazon.com/images/I/41vKhXlhfGL._AC_.jpg",
+    shu: "1.8m SHU"
   }
 ];
